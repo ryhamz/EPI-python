ceee8bfa84677248eeec09361285baca2230768b
Ryan Hamilton <ryhamz@gmail.com>
2018-01-31 23:05:31 -0600

    even odd

---
+++ ch2/examples/even_odd.py
@@ -0,0 +1,10 @@
+def even_odd(nums):
+    next_even, next_odd = 0, len(nums) - 1
+
+    while next_even < next_odd:
+        if nums[next_even] % 2 == 0:
+            next_even += 1
+        else:
+            nums[next_even], nums[next_odd] = nums[next_odd], nums[next_even]
+            next_odd -= 1
+    return nums
